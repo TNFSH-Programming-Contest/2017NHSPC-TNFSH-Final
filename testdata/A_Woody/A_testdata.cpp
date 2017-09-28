c4d761489b39d8f0b057e231d4804ac315b4f469
xiplus <huangxuanyuxiplus@gmail.com>
2017-09-28 20:36:21 +0800

    update A_Woody testdata gen fix pow10
    pow10 -> _pow10

--- testdata/A_Woody/A_testdata.cpp
+++ testdata/A_Woody/A_testdata.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "../testlib.h"
 using namespace std;
-int pow10[]={1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+int _pow10[]={1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 int main(int argc, char* argv[]){
 	registerGen(argc, argv, 1);
 	for(int q=1;q<=2;q++){
@@ -11,8 +11,8 @@ int main(int argc, char* argv[]){
 			fstream f;
 			f.open(ss.str().c_str(), ios::out);
 			
-			int range1 = pow10[rnd.next(1,8)]-1;
-			int range2 = pow10[rnd.next(1,4)]-1;
+			int range1 = _pow10[rnd.next(1,8)]-1;
+			int range2 = _pow10[rnd.next(1,4)]-1;
 			f<<rnd.next(1,range1)<<" "<<rnd.next(1,range2)<<endl;
 			
 			f.close();
