3f3f6aeecb8302daefa44da10c63dd9c5b97a1d2
xiplus <huangxuanyuxiplus@gmail.com>
2017-09-28 17:02:01 +0800

    +B_MagicNumber testdata generator

---
+++ testdata/B_MagicNumber/B_testdata.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+mt19937 g1(11037);
+int RAND(int a, int b){
+	return g1()%(b-a+1)+a;
+}
+int tests[]={0, 33, 33, 34};
+int data[]={0, 5, 1000, 1000000};
+char clist []="0123456789abcdef";
+int main(){
+	for(int q=1;q<=3;q++){
+		for(int w=1;w<=tests[q];w++){
+			stringstream ss;
+			ss<<(q<10?"0":"")<<q<<(w<10?"0":"")<<w<<".in";
+			fstream f;
+			f.open(ss.str().c_str(), ios::out);
+			
+			int t=RAND(1, data[q]);
+			f<<clist[RAND(1, 15)];
+			for(int e=2;e<=t;e++){
+				f<<clist[RAND(0, 15)];
+			}
+			f<<endl;
+			
+			f.close();
+		}
+	}
+}
+
