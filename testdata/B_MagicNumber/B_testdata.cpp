1a67ab598e32087d4544501591ff6bb0421df624
xiplus <huangxuanyuxiplus@gmail.com>
2017-09-30 14:38:12 +0800

    update B testdata gen

--- testdata/B_MagicNumber/B_testdata.cpp
+++ testdata/B_MagicNumber/B_testdata.cpp
@@ -1,32 +1,41 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
 #include "../testlib.h"
 using namespace std;
-//mt19937 g1(11037);
-int RAND(int a, int b){
-//	return g1()%(b-a+1)+a;
-	return rnd.next(a, b);
-}
-int tests[]={0, 33, 33, 34};
-int data[]={0, 5, 1000, 1000000};
-char clist []="0123456789abcdef";
-int main(int argc, char* argv[]){
+int tests[] = {0, 33, 33, 34};
+int data[] = {0, 20, 4000, 4000000};
+char clist[] = "0123456789abcdef";
+bool x[4000000] = {};
+int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
-	for(int q=1;q<=3;q++){
-		for(int w=1;w<=tests[q];w++){
+	for(int q=1; q<=3; q++) {
+		for(int w=1; w<=tests[q]; w++) {
 			stringstream ss;
-			ss<<(q<10?"0":"")<<q<<(w<10?"0":"")<<w<<".in";
+			ss<<"data/"<<setfill('0')<<setw(2)<<q<<setw(2)<<w<<".in";
 			fstream f;
-			f.open(ss.str().c_str(), ios::out);
-			
-			int t=RAND(1, data[q]);
-			f<<clist[RAND(1, 15)];
-			for(int e=2;e<=t;e++){
-				f<<clist[RAND(0, 15)];
+			f.open(ss.str().c_str(), ios::binary|ios::out);
+
+			memset(x, 0, sizeof(x));
+			for(int e=0; e<data[q]; e++) {
+				int s = (data[q]-e)/3;
+				if(s<= 1) break;
+				int c = rnd.next(1, s);
+				e += c - e%c;
+				x[e] = 1;
+				x[e+c] = 1;
+				x[e+2*c] = 1;
+				e += c*3;
+			}
+			int e=data[q]-1;
+			while(x[e]+x[e-1]+x[e-2]+x[e-3]==0) {
+				e-=4;
+			}
+			for(; e>=0; e-=4) {
+				f<<clist[x[e]*8+x[e-1]*4+x[e-2]*2+x[e-3]];
 			}
 			f<<endl;
-			
+
 			f.close();
 		}
 	}
 }
-
---
+++ testdata/B_MagicNumber/gen.sh
@@ -0,0 +1,3 @@
+rm -r data
+mkdir data
+./B_testdata glCEyMQ0fXrxJm60vNHfHRFwG4DG
