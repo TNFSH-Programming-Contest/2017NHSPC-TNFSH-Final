68e54ba1f9fdd98d58ae42e0c9342277dd8288d3
domen111 <domen.public@gmail.com>
2017-10-02 12:10:31 +0800

    fix bug

--- code/domen/D_ENA/ena-45pt.cpp
+++ code/domen/D_ENA/ena-45pt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <complex>
 #include <cmath>
 #include <cassert>
@@ -22,7 +23,10 @@ double cross3(Point a, Point b, Point c)
 double calc_degree(Point a, Point b, Point c)
 {
 	double cosx = dot(a-b, c-b) / (abs(a-b) * abs(c-b));
-	double degree = acos(cosx)*180/acos(-1);
+	double degree = acos(cosx+1e-10)*180/acos(-1);
+	//cout<<fixed<<setprecision(180)<<cosx+1<<' '<<-1<<endl;
+	//cout<<cosx<<' '<<degree<<' '<<acos(cosx)<<' '<<acos(-1)<<endl;
+	assert(!isnan(degree));
 	return 180-degree;
 	//if(cross(b-a, c-b)>0) return 180 - degree;
 	//else return -180 + degree;
@@ -59,28 +63,44 @@ int main()
 		for(int j=0;j<i;j++)
 		{
 			if(!conn[i][j])
+			{
 				dp[i][j]=-INF-2;
+				//assert(j!=i-1);
+			}
 			else if(j==0)
 				dp[i][j]=2;
 			else
 			{
 				dp[i][j]=-INF;
 				for(int k=0;k<j;k++)
+				{
 					if(conn[j][k])
 					{
 						if(calc_degree(p(k), p(j), p(i)) < d)
 							dp[i][j] = max(dp[i][j], dp[j][k] + 1);
+						//cout<<calc_degree(p(k), p(j), p(i))<<endl;
 					}
+					//else if(j==i-1 && k==j-1)
+					//	cout<<k<<' '<<i<<' '<<j<<endl;
+				}
 			}
 		}
 	}
 
 	//for(int i=1;i<n;i++)
 	//{
-	//    cout<<i<<"   ";
-	//    for(int j=0;j<i;j++)
-	//        cout<<dp[i][j]<<' ';
-	//    cout<<endl;
+	//	cout<<i<<"   ";
+	//	for(int j=0;j<i;j++)
+	//		cout<<dp[i][j]<<' ';
+	//	cout<<endl;
+	//}
+	//for(int i=1;i<n;i++)
+	//{
+	//	if(dp[i][i-1]!=i+1)
+	//	{
+	//		cout<<i<<' '<<dp[i][i-1]<<endl;
+	//		break;
+	//	}
 	//}
 
 	int ans=-INF;
